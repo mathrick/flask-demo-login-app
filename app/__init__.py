a357654a1784214b6f3dd225404083838ab46ddb
Maciej Katafiasz <mathrick@gmail.com>
2014-03-21 18:53:43 +0100

    Set things up for SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
 from os import path
 
 app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
 
 # Function to easily find your assets
 # In your template use <link rel=stylesheet href="{{ static('filename') }}">

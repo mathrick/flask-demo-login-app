95349047eca925ac7eec86556bf2ee4260e4614c
Maciej Katafiasz <mathrick@gmail.com>
2014-03-21 16:48:54 +0100

    More robust tests for json_result decorator

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -4,20 +4,26 @@ from werkzeug.datastructures import MIMEAccept, Headers
 from app import decorators
 
 class TestDecorators(FlaskTestCase):
-    def test_decorator_json_result(self):
+    def test_json_result(self):
         assert decorators.json_result
 
         reject_msg = "This is a JSON API endpoint"
 
         rv = self.client.get("/")
-        assert reject_msg in rv.data
+        assert reject_msg in rv.data and rv.status_code >= 400
+
+        def try_reject(*mime_types):
+            accept = MIMEAccept(mime_types)
+            rv = self.client.get("/", headers = Headers([('Accept', accept.to_header())]))
+            return reject_msg in rv.data and rv.status_code >= 400 and rv.mimetype != "application/json"
 
         def try_accept(*mime_types):
             accept = MIMEAccept(mime_types)
-            return reject_msg not in self.client.get("/", headers = Headers([('Accept', accept.to_header())])).data
+            rv = self.client.get("/", headers = Headers([('Accept', accept.to_header())]))
+            return rv.status_code == 200 and rv.mimetype == "application/json"
 
-        assert not try_accept(("*/*", 1))
-        assert not try_accept(("text/html", 1), ("*/*", 0.8))
+        assert try_reject(("*/*", 1))
+        assert try_reject(("text/html", 1), ("*/*", 0.8))
 
         assert try_accept(("application/json", 1), ("*/*", 0.8))
         assert try_accept(("text/html", 1), ("application/json", 0.9), ("*/*", 0.8))

0afa0a2c10fd0992560e123c8a036dc528d2049a
Maciej Katafiasz <mathrick@gmail.com>
2014-03-24 14:29:43 +0100

    Add a 'test' command to run.py, so setup.py is no longer necessary just to fire the test suite.

--- run.py
+++ run.py
@@ -2,9 +2,11 @@
 from flask.ext.script import Manager, Server
 from flask.ext.migrate import Migrate, MigrateCommand
 from app import app, db
+import unittest
 
 migrate = Migrate(app, db, directory=app.config['ALEMBIC_MIGRATE_DIR'])
 
+# Wire up the DB for normal server run
 class MyServer(Server):
     def run(self, *args, **kwargs):
         db.init_app(app)
@@ -12,6 +14,14 @@ class MyServer(Server):
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+manager.add_command('runserver', MyServer)
+
+# Run the testsuite instead of the normal server
+@manager.command
+def test():
+    t = unittest.defaultTestLoader.discover(".")
+    runner = unittest.runner.TextTestRunner()
+    runner.run(t)
 
 if __name__ == "__main__":
     manager.run(default_command="runserver")

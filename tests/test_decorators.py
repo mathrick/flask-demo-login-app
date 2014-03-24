dfaf16d19d6cb80e666cfa5ef7900f789154fae7
Maciej Katafiasz <mathrick@gmail.com>
2014-03-24 11:50:08 +0100

    Use proper unittest assert* methods in tests
    Can't use plain assert, or unittest won't collect failures for reports.

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -5,12 +5,12 @@ from app import decorators
 
 class TestDecorators(FlaskTestCase):
     def test_json_result(self):
-        assert decorators.json_result
+        self.assertTrue(decorators.json_result)
 
         reject_msg = "This is a JSON API endpoint"
 
         rv = self.client.get("/")
-        assert reject_msg in rv.data and rv.status_code >= 400
+        self.assertTrue(reject_msg in rv.data and rv.status_code >= 400)
 
         def try_reject(*mime_types):
             accept = MIMEAccept(mime_types)
@@ -22,8 +22,8 @@ class TestDecorators(FlaskTestCase):
             rv = self.client.get("/", headers = Headers([('Accept', accept.to_header())]))
             return rv.status_code == 200 and rv.mimetype == "application/json"
 
-        assert try_reject(("*/*", 1))
-        assert try_reject(("text/html", 1), ("*/*", 0.8))
+        self.assertTrue(try_reject(("*/*", 1)))
+        self.assertTrue(try_reject(("text/html", 1), ("*/*", 0.8)))
 
-        assert try_accept(("application/json", 1), ("*/*", 0.8))
-        assert try_accept(("text/html", 1), ("application/json", 0.9), ("*/*", 0.8))
+        self.assertTrue(try_accept(("application/json", 1), ("*/*", 0.8)))
+        self.assertTrue(try_accept(("text/html", 1), ("application/json", 0.9), ("*/*", 0.8)))

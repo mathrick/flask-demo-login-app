a9fec273b7515245f8bf7f6b6f4bf8024c65a8c0
Maciej Katafiasz <mathrick@gmail.com>
2014-03-24 14:48:58 +0100

    Set up the DB in test fixtures.

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,6 +4,13 @@ from app import app, db
 from flask.ext.testing import TestCase
 
 class FlaskTestCase(TestCase):
+    def setUp(self):
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        
     def create_app(self):
         app.config.from_object(TestConfig)
         db.init_app(app)

6b6e1f4017c5209cf7c1f3b7aa94b3ac7ea7b64c
Maciej Katafiasz <mathrick@gmail.com>
2014-03-24 09:44:55 +0100

    Add a Message model for users to have something when they log in

--- app/models.py
+++ app/models.py
@@ -4,5 +4,16 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(50), index = True, unique = True)
     email = db.Column(db.String(100), index = True, unique = True)
-
+    messages = db.relationship('message', backref = 'recipient', lazy = 'dynamic')
     
+    def __repr__(self):
+        return "<User %s>" % self.name
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), index = True)
+    text = db.Column(db.Text)
+
+    timestamp = db.Column(db.DateTime, server_default = db.func.current_timestamp())
+    unread = db.Column(db.Boolean, default = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable = False)

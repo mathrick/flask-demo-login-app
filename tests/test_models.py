855ea582c1d49d26265797c085faab40cc7fe329
Maciej Katafiasz <mathrick@gmail.com>
2014-03-25 09:48:10 +0100

    Add timestamp testing to model tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,15 @@ from . import FlaskTestCase
 from unittest import skip, skipIf
 from app import db
 from app.models import User, Message
+from datetime import datetime, timedelta
+
+# These tests are necessarily very rudimentary, as the application and
+# its models do very little, so we're mostly testing SQLAlchemy. But
+# that is still valuable, as it serves to make sure we're using
+# SQLAlchemy properly (it's a really flexible package and it's easy to
+# do something you're not supposed to) and at the same time, or that
+# we don't have something specific to the RDBMS used when porting to
+# another one
 
 class TestUser(FlaskTestCase):
     def test_no_users_in_empty_db(self):
@@ -31,13 +40,19 @@ class TestMessage(FlaskTestCase):
         self.assertEqual(0, len(Message.query.all()))
 
     def test_make_welcome_message(self):
-        pass
         arthur = User.query.filter_by(name = "King Arthur").first()
+        timestamp = datetime.utcnow()
+
         db.session.add(Message(recipient = arthur,
                                title = "Welcome!",
                                text = "Hello, %s!\nWelcome to zombo.com" % arthur.name))
-        
         db.session.commit()
 
         self.assertEqual(1, len(Message.query.all()))
         self.assertEqual(1, len(Message.query.filter_by(recipient = arthur).all()))
+
+        msg = Message.query.filter_by(recipient = arthur).first()
+        self.assertTrue(msg.unread)
+        # Give up to two seconds of leeway in timestamps, as utcnow()
+        # only operates on second precision
+        self.assertLessEqual(abs(msg.timestamp - timestamp), timedelta(0,2))

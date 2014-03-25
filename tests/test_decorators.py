749acb6cb6a0fb1e261f56be2dfd8be9bbe10669
Maciej Katafiasz <mathrick@gmail.com>
2014-03-25 13:40:45 +0100

    Update decorator tests

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -4,18 +4,19 @@ from werkzeug.datastructures import MIMEAccept, Headers
 from app import decorators
 
 class TestDecorators(FlaskTestCase):
+    url="/api/test"
     def test_json_decorator_defined(self):
         self.assertTrue(decorators.json_result)
 
     def test_json_rejects_html_clients(self):
         reject_msg = "This is a JSON API endpoint"
-
-        rv = self.client.get("/")
+        
+        rv = self.client.get(self.url)
         self.assertTrue(reject_msg in rv.data and rv.status_code >= 400)
 
         def try_reject(*mime_types):
             accept = MIMEAccept(mime_types)
-            rv = self.client.get("/", headers = Headers([('Accept', accept.to_header())]))
+            rv = self.client.get(self.url, headers = Headers([('Accept', accept.to_header())]))
             return reject_msg in rv.data and rv.status_code >= 400 and rv.mimetype != "application/json"
 
         self.assertTrue(try_reject(("*/*", 1)))
@@ -24,7 +25,7 @@ class TestDecorators(FlaskTestCase):
     def test_json_decorator_returns_json(self):
         def try_accept(*mime_types):
             accept = MIMEAccept(mime_types)
-            rv = self.client.get("/", headers = Headers([('Accept', accept.to_header())]))
+            rv = self.client.get(self.url, headers = Headers([('Accept', accept.to_header())]))
             return rv.json
 
         self.assertTrue(try_accept(("application/json", 1), ("*/*", 0.8)))

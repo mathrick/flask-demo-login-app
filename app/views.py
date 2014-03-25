aacdf692da6a7eb531ba24b9f8a0cc200c83b64b
Maciej Katafiasz <mathrick@gmail.com>
2014-03-25 21:42:07 +0100

    Add the views and templates where login/signup forms will live, and a navbar in the base template.

--- app/views.py
+++ app/views.py
@@ -6,6 +6,14 @@ from app.decorators import json_result
 def index():
     return render_template("index.html")
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
+    
+@app.route('/sign-up', methods=["GET", "POST"])
+def sign_up():
+    return render_template("sign-up.html")
+    
 @app.route('/api/test')
 @json_result
 def api_test():

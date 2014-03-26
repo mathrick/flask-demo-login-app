056d5eeea78fc295778dc771546e1351c25af3bd
Maciej Katafiasz <mathrick@gmail.com>
2014-03-26 13:57:56 +0100

    Allow non-unique names for User

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(50), index = True, unique = True)
+    name = db.Column(db.String(50), index = True)
     email = db.Column(db.String(100), index = True, unique = True)
     pw_hash = db.Column(db.String(60), index = True)
     messages = db.relationship('Message', backref = 'recipient', lazy = 'dynamic')

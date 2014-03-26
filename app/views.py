23bed4e3f0918aee64156c4902027f0406bad78e
Maciej Katafiasz <mathrick@gmail.com>
2014-03-26 13:58:25 +0100

    Create a new user on successful sign up

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from flask import render_template, url_for, redirect
 from app import db, app, forms, models
+from sqlalchemy.exc import IntegrityError
+from app.password import bcrypt
 from app.decorators import json_result
 
 @app.route('/')
@@ -17,12 +19,19 @@ def login():
 def sign_up():
     form = forms.SignUpForm()
     if form.validate_on_submit():
-        make_new_user(form)
-        return redirect(url_for('index'))
+        try:
+            make_new_user(form)
+            return redirect(url_for('index'))
+        except IntegrityError:
+            form.email.errors += ['This email has been registered already']
     return render_template("sign-up.html", form=form)
 
 def make_new_user(form):
-    pass
+    u = models.User(name=form.name.data,
+                    email=form.email.data,
+                    pw_hash=bcrypt.generate_password_hash(form.password.data))
+    db.session.add(u)
+    db.session.commit()
 
 @app.route('/api/test')
 @json_result

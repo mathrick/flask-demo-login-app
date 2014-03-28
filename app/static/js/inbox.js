824a5a24579b2d777916472af013baa6efba09a0
Maciej Katafiasz <mathrick@gmail.com>
2014-03-28 10:56:33 +0100

    Fetch the message list via REST API

--- app/static/js/inbox.js
+++ app/static/js/inbox.js
@@ -1,12 +1,8 @@
-function InboxCtrl($scope) {
-    $scope.messages = [{ title: "angular",
-                         text: "is working",
-                         timestamp: "today",
-                         id: 15
-                       },
-                       { title: "again",
-                         text: "is working",
-                         timestamp: "today",
-                         id: 71
-                       }]
-}
+var InboxApp = angular.module('InboxApp', []);
+
+InboxApp.controller('InboxCtrl', 
+                    ['$scope', '$http', function InboxCtrl ($scope, $http) {
+                        $http.get('/api/message/').success(function(data) {
+                            $scope.messages = data;
+                        });
+                    }]);

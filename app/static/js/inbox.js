b9ed9f067e7becb676881d5375599ec566050cef
Maciej Katafiasz <mathrick@gmail.com>
2014-03-28 11:43:06 +0100

    Display individual messages with AngularJS

--- app/static/js/inbox.js
+++ app/static/js/inbox.js
@@ -1,8 +1,33 @@
-var InboxApp = angular.module('InboxApp', []);
+var InboxApp = angular.module('InboxApp', ['ngRoute']);
 
 InboxApp.controller('InboxCtrl', 
-                    ['$scope', '$http', function InboxCtrl ($scope, $http) {
-                        $http.get('/api/message/').success(function(data) {
-                            $scope.messages = data;
-                        });
-                    }]);
+                    ['$scope', '$http',
+                     function InboxCtrl ($scope, $http) {
+                         $http.get('/api/message/').success(function(data) {
+                             $scope.messages = data;
+                         });
+                     }]);
+
+InboxApp.controller('MessageCtrl', 
+                    ['$scope', '$http', '$routeParams', 
+                     function InboxCtrl ($scope, $http, $params) {
+                         $http.get('/api/message/'+ $params.messageId).success(function(data) {
+                             $scope.message = data;
+                         });
+                     }])
+
+InboxApp.config(['$routeProvider', function($route) {
+    $route
+        .when('/message/:messageId', {
+            templateUrl: '/static/angular/message.html',
+            controller: 'MessageCtrl'
+        })
+        .when('/inbox/', {
+            templateUrl: '/static/angular/inbox.html',
+            controller: 'InboxCtrl'
+        })
+        .otherwise({
+            templateUrl: '/static/angular/inbox.html',
+            controller: 'InboxCtrl'
+        })
+}])
